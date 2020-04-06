a7333a71e8b2bc8547b6328bb9794d96610e5201
Sérgio Ferreira <sfer96@gmail.com>
2020-04-07 00:55:47 +0100

    Issue 8
    changed mapping system

--- prompt.sh
+++ prompt.sh
@@ -6,44 +6,99 @@
 # author     : sergio ferreira <sfer96@gmail.com>
 # date       : 20200310
 # ============================================================================
+# PS1 formats
 case "${_shtype}" in
-
 	"bash")
-		declare -A PROMPT_COLORS
-
-		PROMPT_COLORS[NORMAL]='\033[0m'
-		PROMPT_COLORS[RED]="\[\e[1;31m\]"
-		PROMPT_COLORS[GREEN]="\[\e[1;32m\]"
-		PROMPT_COLORS[YELLOW]="\[\e[1;33m\]"
-		PROMPT_COLORS[BLUE]="\[\e[1;34m\]"
-		PROMPT_COLORS[PINK]="\[\e[1;35m\]"
-		PROMPT_COLORS[CYAN]="\[\e[1;36m\]"
-		PROMPT_COLORS[WHITE]="\[\e[1;37m\]"
-
-		PS1="${PROMPT_COLORS[BLUE]}\u@\H:\w [${PROMPT_COLORS[CYAN]}\t${PROMPT_COLORS[BLUE]}]\$ ${PROMPT_COLORS[NORMAL]}"
+		_prefix='\[\033['
+		_suffix='m\]'
 	;;
 	"ksh")
-		color=(
-			black='\033[30m' \
-			red='\033[31m' \
-			green='\033[32m' \
-			brown='\033[33m' \
-			blue='\033[34m' \
-			purple='\033[35m' \
-			cyan='\033[36m' \
-			lightgray='\033[37m' \
-			darkgray='\033[1;30m' \
-			lightred='\033[1;31m' \
-			lightgreen='\033[1;32m' \
-			yellow='\033[1;33m' \
-			lightblue='\033[1;34m' \
-			pink='\033[1;35m' \
-			lightcyan='\033[1;36m' \
-			white='\033[1;37m' \
-			auto='\033[0m'
-		)
+		_prefix='\033['
+		_suffix='m'
+	;;
+esac
 
-		PS1=$( print -n "${color.lightblue}${USER}@${HOSTNAME}:\${PWD}${color.lightcyan} [\$(date +"%H:%M:%S")]${color.lightblue}$ ${color.auto}" )
+
+# text format codes
+typeset -A _text_formats=(
+	['normal']='00'
+	['bold']='01'
+	['underline']="04"
+	['flash']="05"
+#	['reverse']=""
+)
+
+
+# text color codes
+typeset -A _basic_colors=(
+	['auto']='00'
+	['black']='30'
+	['red']='31'
+	['green']='32'
+	['yellow']='33'
+	['blue']='34'
+	['pink']='35'
+	['cyan']='36'
+	['white']='37'
+)	
+
+
+# prompt colors formated
+# 	b_: bold
+# 	u_: underline
+# 	f_: flash / blink
+typeset -A _prompt_colors=(
+	
+	['auto']="\033[0m"		# common to both bash and ksh
+	
+	['black']="${_prefix}${_text_formats['normal']};${_basic_colors['black']}${_suffix}"
+	['b_black']="${_prefix}${_text_formats['bold']};${_basic_colors['black']}${_suffix}"
+	['u_black']="${_prefix}${_text_formats['underline']};${_basic_colors['black']}${_suffix}"
+	['f_black']="${_prefix}${_text_formats['flash']};${_basic_colors['black']}${_suffix}"
+	
+	['white']="${_prefix}${_text_formats['normal']};${_basic_colors['white']}${_suffix}"
+	['b_white']="${_prefix}${_text_formats['bold']};${_basic_colors['white']}${_suffix}"
+	['u_white']="${_prefix}${_text_formats['underline']};${_basic_colors['white']}${_suffix}"
+	['f_white']="${_prefix}${_text_formats['flash']};${_basic_colors['white']}${_suffix}"
+	
+	['red']="${_prefix}${_text_formats['normal']};${_basic_colors['red']}${_suffix}"
+	['b_red']="${_prefix}${_text_formats['bold']};${_basic_colors['red']}${_suffix}"
+	['u_red']="${_prefix}${_text_formats['underline']};${_basic_colors['red']}${_suffix}"
+	['f_red']="${_prefix}${_text_formats['flash']};${_basic_colors['red']}${_suffix}"
+	
+	['green']="${_prefix}${_text_formats['normal']};${_basic_colors['green']}${_suffix}"
+	['b_green']="${_prefix}${_text_formats['bold']};${_basic_colors['green']}${_suffix}"
+	['u_green']="${_prefix}${_text_formats['underline']};${_basic_colors['green']}${_suffix}"
+	['f_green']="${_prefix}${_text_formats['flash']};${_basic_colors['green']}${_suffix}"
+	
+	['yellow']="${_prefix}${_text_formats['normal']};${_basic_colors['yellow']}${_suffix}"
+	['b_yellow']="${_prefix}${_text_formats['bold']};${_basic_colors['yellow']}${_suffix}"
+	['u_yellow']="${_prefix}${_text_formats['underline']};${_basic_colors['yellow']}${_suffix}"
+	['f_yellow']="${_prefix}${_text_formats['flash']};${_basic_colors['yellow']}${_suffix}"
+	
+	['blue']="${_prefix}${_text_formats['normal']};${_basic_colors['blue']}${_suffix}"
+	['b_blue']="${_prefix}${_text_formats['bold']};${_basic_colors['blue']}${_suffix}"
+	['u_blue']="${_prefix}${_text_formats['underline']};${_basic_colors['blue']}${_suffix}"
+	['f_blue']="${_prefix}${_text_formats['flash']};${_basic_colors['blue']}${_suffix}"
+	
+	['pink']="${_prefix}${_text_formats['normal']};${_basic_colors['pink']}${_suffix}"
+	['b_pink']="${_prefix}${_text_formats['bold']};${_basic_colors['pink']}${_suffix}"
+	['u_pink']="${_prefix}${_text_formats['underline']};${_basic_colors['pink']}${_suffix}"
+	['f_pink']="${_prefix}${_text_formats['flash']};${_basic_colors['pink']}${_suffix}"
+	
+	['cyan']="${_prefix}${_text_formats['normal']};${_basic_colors['cyan']}${_suffix}"
+	['b_cyan']="${_prefix}${_text_formats['bold']};${_basic_colors['cyan']}${_suffix}"
+	['u_cyan']="${_prefix}${_text_formats['underline']};${_basic_colors['cyan']}${_suffix}"
+	['f_cyan']="${_prefix}${_text_formats['flash']};${_basic_colors['cyan']}${_suffix}"
+)
+
+
+case "${_shtype}" in
+	"bash")
+		PS1="${_prompt_colors['blue']}\u@\H:\w [${_prompt_colors['cyan']}\t${_prompt_colors['blue']}] \$ ${_prompt_colors[auto]}"
+	;;
+	"ksh")
+		PS1=$( print -n "${_prompt_colors['blue']}${USER}@${HOSTNAME}:\${PWD} [${_prompt_colors['cyan']}\$(date +"%H:%M:%S")${_prompt_colors['blue']}] $ ${_prompt_colors['auto']}" )
 	;;
 esac
 
